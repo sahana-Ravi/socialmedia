15e5dd62c93b72e83bfb44645efd867cf64a072f
unknown <shvesa99@gmail.com>
2022-11-19 16:06:46 +0530

    'f'

--- social/models.py
+++ social/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
+import allauth
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
